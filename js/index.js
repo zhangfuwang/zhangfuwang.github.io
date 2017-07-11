03789e34e3a6053f5e388755b8557529655c60c3
zhangfuwang <zhang_xu6941@163.com>
2017-07-11 15:39:51 +0800

    school

--- js/index.js
+++ js/index.js
@@ -31,7 +31,43 @@ $(function(){
 //                });
     }
 
-    //下拉框
+    //二级联动下拉框
+    //var provinceList = [
+    //    '北京市',
+    //    '上海市',
+    //    '浙江省'
+    //];
+    //var cityList = [
+    //    ['东城区', '西城区', '海淀区'],
+    //    ['闵行区', '浦东区', '金山区'],
+    //    ['杭州市', '宁波市', '绍兴市', '嘉兴市']
+    //];
+    ////读取每个省份名称，生成一个option，追加到select
+    //var html = '';
+    //for (var i = 0; i < provinceList.length; i++) {
+    //    var p = provinceList[i];
+    //    html += `<option value="${i}">${p}</option>`;
+    //}
+    //$('#select-province').append(html);
+    //
+    ////为省份select添加“选项改变”事件绑定
+    //$('#select-province').change(function(){
+    //    console.log($(this).val());//select的value就是当前选中的option的value
+    //    console.log(this.value);//select的value就是当前选中的option的value
+    //    if(this.value==='-1'){
+    //        return; //当前用户选定了“请选择城市”
+    //    }
+    //    //获取当前选定的省份的地级市列表
+    //    var cities = cityList[ this.value ];
+    //    console.log(cities);
+    //    var html = '<option value="-1">—请选择地级市—</option>';
+    //    for(var i=0; i<cities.length; i++){
+    //        var c = cities[i];
+    //        html+=`<option value="${i}">${c}</option>`;
+    //    }
+    //    //$('#select-city').append(html);
+    //    $('#select-city').html(html);
+    //});
 
 
     $(" div.spinner i ").click(function() {
@@ -39,65 +75,75 @@ $(function(){
         //console.log($(this).next().children());
          $(this).next().children().mouseenter(function(){
                         $(this).addClass("active").siblings(".active").removeClass("active");
-                }).click(function(){
-                    //var liHtml=$(this).children().html();
+                }).click(function(e){
+                    e.preventDefault();
+                    //console.log("1");
+                    var liHtml=$(this).children().html();
                     if($(this).parent().siblings(" #schoolInput ").attr("placeholder")==="学校"){
-                        location.href="schoolEvaluation.html";
-                    //    $(" #schoolInput ").val(liHtml);
+                        //location.href="schoolEvaluation.html";
+                        //console.log("1");
+                        $(" #schoolInput ").val(liHtml);
                     }else{
-                    //    $(" #curriculumInput ").val(liHtml)
-                        location.href="curriculumEvaluation.html";
+                        //console.log("1");
+                        $(" #curriculumInput ").val(liHtml);
+
+                        //location.href="curriculumEvaluation.html";
                     }
 
          })
     });
-    $(" #schoolSearch i").click(function(){
-        $(this).parent().hide().next().show();
-    });
-    $(" #schoolSearch span ").click(function(e){
-        //console.log("hello");
-        e.preventDefault();
-        location.href="schoolEvaluation.html";
-    });
-    $(" #curriculumSearch span ").click(function(e){
-        e.preventDefault();
-        location.href="curriculumEvaluation.html";
-    });
-    $(" #curriculumSearch i").click(function(){
-        $(this).parent().hide().prev().show();
-    });
+    $(" #indexSearch ").click(function(){
+        var input=$(this).parent().siblings(".search").children().children(".input");
+        //console.log(input);
+        for(var i=0;i<input.length;i++){
+            console.dir(input[0].value);
+            if(input[0].value!=="" && input[1].value===""){
+                //console.log("1");
+                location.href="schoolComment.html";
+            }else if(input[0].value==="" && input[1].value!==""){
+                //console.log("2");
+                location.href="curriculumComment.html";
+            }else if(input[0].value!=="" && input[1].value!==""){
+                //console.log("3");
+                location.href="schoolComment.html";
+            }else{
+                //console.log("4");
+                location.href="index.html";
+            }
 
-    //查看更多按钮隐藏
-    //$(" button.more").click(function(e){
-    //    e.preventDefault();
-    //   $(this).hide();
-    //});
-
-
-
-
-    //$(" div.spinner ").hover(function(){
-    //    $(this).children().last().show();
-    //
-    //    $(this).children().last().children().mouseenter(function(){
-    //        console.log($(this));
-    //
-    //            $(this).addClass("active").siblings(".active").removeClass("active");
-    //
-    //    }).click(function(){
-    //        location.href="schoolEvaluation.html";
-    //    })
-    //
-    //},function(){
-    //    $(this).children().last().hide();
-    //});
-
-    $("#search").next().click(function(){
-        var search=$("#search").val();
-        $.ajax({
-
-        })
+        }
+        //if(input.attr("id")==="schoolInput"){
+        //    //location.href="schoolComment.html";
+        //}else if(input.attr("id")==="curriculumInput"){
+        //    //location.href="curriculumComment.html";
+        //
+        //}
     })
+
+    //$(" #schoolSearch i").click(function(){
+    //    $(this).parent().hide().next().show();
+    //});
+    //$(" #schoolSearch span ").click(function(e){
+    //    //console.log("hello");
+    //    e.preventDefault();
+    //    location.href="schoolEvaluation.html";
+    //});
+    //$(" #curriculumSearch span ").click(function(e){
+    //    e.preventDefault();
+    //    location.href="curriculumEvaluation.html";
+    //});
+    //$(" #curriculumSearch i").click(function(){
+    //    $(this).parent().hide().prev().show();
+    //});
+
+
+
+    //$("#search").next().click(function(){
+    //    var search=$("#search").val();
+    //    $.ajax({
+    //
+    //    })
+    //})
 
 
 });
